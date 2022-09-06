0ac7e37ffc0d96d5eb4adb26a44d0b96cdbefc81
Caleb Ukle <me+info@calebukle.com>
2022-09-06 16:44:21 -0500

    some search input

--- apps/calebukle-com/astro.config.mjs
+++ apps/calebukle-com/astro.config.mjs
@@ -3,9 +3,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://calebukle.com',
-	integrations: [mdx(), sitemap(), tailwind()],
-	outDir: '../../dist/apps/calebukle-com',
+  site: 'https://calebukle.com',
+  integrations: [mdx(), sitemap(), tailwind(), svelte()],
+  outDir: '../../dist/apps/calebukle-com'
 });
