55c46aae43729fb873fc1a48c08f34496df896b1
Caleb Ukle <me+info@calebukle.com>
2023-02-08 09:24:41 -0600

    update schema

--- apps/coffee-cms/schemas/beans.ts
+++ apps/coffee-cms/schemas/beans.ts
@@ -1,38 +1,23 @@
-export default {
-  name: 'beans',
-  type: 'document',
-  title: 'Beans',
-  fields: [
-    {
-      type: 'string',
-      name: 'name',
-      title: 'Name',
-    },
-    {
-      type: 'string',
-      name: 'roaster',
-      title: 'Roaster',
-    },
-    {
-      type: 'string',
-      name: 'Seller',
-      title: 'Seller',
-    },
-    {
-      type: 'url',
-      name: 'url',
-      title: 'Url',
-    },
-    {
-      type: 'image',
-      name: 'image',
-      title: 'Image',
-    },
-    {
-      type: 'array',
-      name: 'info',
-      title: 'Info',
-      of: [{type: 'block'}]
-    }
-  ]
-}
+import {defineArrayMember, defineField, defineType} from 'sanity'
+
+export default defineType({
+	type: 'document',
+	name: 'beans',
+	title: 'Beans',
+	fields: [
+		defineField({name: 'name', type: 'string', title: 'Name'}),
+		defineField({name: 'roaster', type: 'string', title: 'Roaster'}),
+		defineField({name: 'seller', type: 'string', title: 'Seller'}),
+		defineField({name: 'url', type: 'url', title: 'Url'}),
+		defineField({name: 'image', type: 'image', title: 'Image'}),
+		defineField({name: 'price', type: 'number', title: 'Price (in Cents)'}),
+		defineField({name: 'weight', type: 'number', title: 'Weight (in Grams)'}),
+		defineField({name: 'order_date', type: 'date', title: 'Order Date'}),
+		defineField({
+			name: 'info',
+			type: 'array',
+			title: 'Info',
+			of: [defineArrayMember({type: 'block'})],
+		}),
+	],
+})
---
+++ apps/coffee-cms/schemas/index.ts
@@ -0,0 +1,4 @@
+import beans from './beans'
+import brew from './brew'
+
+export const schemaTypes = [brew, beans]
